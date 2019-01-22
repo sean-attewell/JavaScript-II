272c275abe36189be122ca922da982c767e33c0e
sean-attewell <sean.attewell@gmail.com>
2019-01-22 17:15:21 +0000

    closure Qs done

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,11 +1,41 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function outerscope() {
+  let invar = "this is from the outer."
+  return function innerscope() {
+    console.log(`inner grabbing from the outer: ${invar}`)
+  }
+}
+
+let parentReturnsInnerFunction = outerscope();
+
+parentReturnsInnerFunction()
+
 
 // ==== Challenge 2: Create a counter function ====
 const counter = () => {
   // Return a function that when invoked increments and returns a counter variable.
+  let score = 0;
+  return function inner() {
+    return ++ score;
+  }
 };
+
+const newcounter = counter();
+
+console.log(newcounter()); 
+console.log(newcounter()); 
+console.log(newcounter()); 
+console.log(newcounter()); 
+console.log(newcounter()); 
+
+const newcounter2 = counter();
+
+console.log(newcounter2())
+console.log(newcounter2())
+console.log(newcounter2())
+
 // Example usage: const newCounter = counter();
 // newCounter(); // 1
 // newCounter(); // 2
