87571f12c41605903b5e49cbc79ab2d5b147c65c
sean-attewell <sean.attewell@gmail.com>
2019-01-22 16:11:35 +0000

    array-methods Qs done

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -1,4 +1,5 @@
-// A local community center is holding a fund rasising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to their facilities.  Each business has assigned a representative to attend the event along with a small donation.
+// A local community center is holding a fund rasising 5k fun run and has invited 50 small businesses to make a small donation on their behalf for some much needed updates to 
+// their facilities.  Each business has assigned a representative to attend the event along with a small donation.
 
 // Scroll to the bottom of the list to use some advanced array methods to help the event director gather some information from the businesses.
 
@@ -56,28 +57,73 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+
+runners.forEach(element => {
+    fullName.push(`${element.first_name} ${element.last_name}`)
+})
+
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
-// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
-let allCaps = [];
+// The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and 
+// log the result
+// let allCaps = [];
+
+let allCaps = runners.map(element => element.first_name.toUpperCase())
+
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
-// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
-let largeShirts = [];
+// The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. 
+// Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
+// let largeShirts = [];
+
+let largeShirts = runners.filter(element => element.shirt_size === "L")
+
 console.log(largeShirts);
+
+
+
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
-let ticketPriceTotal = [];
+// let ticketPriceTotal = [];
+
+
+// **WORKING**
+function adder(acc, currentPerson) {
+    return acc + currentPerson.donation;
+  }
+
+const ticketPriceTotal = runners.reduce(adder, 0)
+
 console.log(ticketPriceTotal);
 
+
+
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 - return all company names, but make each start with the "The" and end with "company" 
 
-// Problem 2
+let companyNames = []
 
-// Problem 3
+runners.forEach(element => companyNames.push(`The ${element.company_name} company`));
+
+console.log(companyNames)
+
+
+// Problem 2 - count how many people made a donation under 100 for public shaming
+
+let under100 = runners.filter(element => element.donation < 100)
+
+console.log(under100)
+console.log(under100.length)
+
+
+// Problem 3 - pump the numbers. Produce a new array where you artificially raising all donations by 100.
+
+let dodgyFigures = runners.map(element => element.donation += 100);
+
+console.log(dodgyFigures);
